d1fa59088e7c0d4cc7b1eb6376facdf6c60f81b5
Filip Korus <filipkorus123@gmail.com>
2022-03-26 17:35:18 +0100

    lab3 ex3 solved

--- lab3/z3.py
+++ lab3/z3.py
@@ -1,2 +1,25 @@
+from math import sin, sqrt
+import random
+
+def definite_integral(a, b, func):
+	N = 100000  # number of random points: the bigger => the better result
+
+	dx = b - a  # distance between a & b
+	s = 0
+	for _ in range(N):
+		random_position = random.uniform(a, b)  # random float from range (a, b)
+		s += func(random_position)
+	s = dx * s / N  # number of points inside rectangle to all points ratio multiplied by range between a & b
+	return s
+
+
 if __name__ == '__main__':
-	pass
+	print(
+		'area of circle with radius of 1 is',
+		4 * definite_integral(0, 1, lambda r: sqrt(1 - r * r))
+	)
+
+	print(
+		'definite integral of sin(x) in range (0, 2) is:',
+		definite_integral(0, 2, sin)
+	)
