056f50df9a7ded38a39cadc5b023e2c84795e58d
Filip Korus <filipkorus123@gmail.com>
2022-03-29 23:45:48 +0200

    lab3/ex3 upgraded

--- lab3/z3.py
+++ lab3/z3.py
@@ -1,23 +1,26 @@
 from math import sin
 import random
 
-def def_integral(a, b, is_inside):
+
+def def_integral(a, b, is_inside, R):
 	N = 1000000
 	s = 0
 	for _ in range(N):
 		x = random.uniform(a, b)
-		y = random.uniform(-5, 5)
+		y = random.uniform(-R, R)
 		if is_inside(x, y):
 			s += 1
-	return (b - a) * 10 * s / N
+	return 2 * R * (b - a) * s / N
+
 
 if __name__ == '__main__':
+	radius = 1
 	print(
-		'area of circle with radius of 1 is',
-		2 * def_integral(0, 1, lambda x, y: x**2 + y**2 < 1)
+		f'area of circle with radius of {radius} is',
+		2 * def_integral(0, radius, lambda x, y: x**2 + y**2 < radius**2, radius)
 	)
 
 	print(
 		'definite integral of sin(x) in range (0, 2) is',
-		def_integral(0, 2, lambda x, y: sin(x) > y > 0)
+		def_integral(0, 2, lambda x, y: sin(x) > y > 0, 1)
 	)
